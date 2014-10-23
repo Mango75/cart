0bbf15680ef97275d55cf4b739bafa3816430667
Magnus Hansson <Nexia@Nexia-Revisions-iMac.local>
2014-10-23 09:03:52 +0200

    In the middle of getting functions right

--- inc/functions.php
+++ inc/functions.php
@@ -13,7 +13,32 @@
      }
 function add_products_to_cart($product){
 	$cartArray=array();
-	foreach($product as $objkey => $objvalue){
+		
+		if(isset($_SESSION['cart'])){
+			foreach($product as $objkey => $objvalue){
+			if($objvalue>0){
+			$db =new Database;
+			$rows=$db->get_product_info($objkey);
+			while($rowinfo=mysqli_fetch_array($rows)){
+				$pro= new Product;
+				$pro->set_all_attr($rowinfo['PrID'], $rowinfo['PrName'], $rowinfo['PrPrice'],$objvalue,$rowinfo['PrPic']);
+				foreach($_SESSION['cart'] as $cartitem)
+				if($objkey==$cartitem->get_id()){
+					$cartitem->set_quantity($objvalue);
+					array_push($cartArray,$cartitem);
+				}
+				else{
+					
+						    }
+			}
+
+			}else{
+			continue;
+			}
+		}
+		}
+		else{
+			foreach($product as $objkey => $objvalue){
 			if($objvalue>0){
 			$db =new Database;
 			$rows=$db->get_product_info($objkey);
@@ -27,12 +52,6 @@ function add_products_to_cart($product){
 			continue;
 			}
 		}
-		if(isset($_SESSION['cart'])){
-			foreach ($product as $key => $value) {
-				# code... for updating use in_array function
-				#or double loops
-			}
-
 		}
 	$_SESSION['cart']=$cartArray;
 
